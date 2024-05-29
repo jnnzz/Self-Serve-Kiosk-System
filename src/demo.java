5ff6cf0319c319adc2bd9282547213c09b0deaea
jnnzz <larocojanlorenz@gmail.com>
2024-05-29 11:22:30 +0800

    12

--- src/demo.java
+++ src/demo.java
@@ -1,6 +1,8 @@
 //package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -21,12 +23,13 @@ public class demo extends Application {
     public void start(Stage stage) throws Exception {
 
         //Stage stage = new Stage();
-        Group root = new Group();
-        Scene scene = new Scene(root,Color.BLACK);
+        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+//        Group root = new Group();
+        Scene scene = new Scene(root);
 
-        //Image icon = new Image("icon.png");
-        //stage.getIcons().add(icon);
-        //stage.setTitle("Stage Demo Program w00t w00t");
+//        Image icon = new Image("first.png");
+//        stage.getIcons().add(icon);
+//        stage.setTitle("Stage Demo Program w00t w00t");
         //stage.setWidth(420);
         //stage.setHeight(420);
         //stage.setResizable(false);
@@ -35,7 +38,7 @@ public class demo extends Application {
         //stage.setFullScreen(true);
         //stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press q");
         //stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
-
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
--- src/guiswing.java
+++ src/guiswing.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class guiswing {
+public class guiswing {
+    public static void main(String[] args){
+
+    }
 }
